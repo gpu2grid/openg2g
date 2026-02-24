1fa3c86c02ba4b2eae8913c1dd9fb251fc3e5243
Jae-Won Chung <jwnchung@umich.edu>
2026-02-24 00:10:23 -0500

    v0.0.1 release

--- openg2g/__init__.py
+++ openg2g/__init__.py
@@ -1,8 +1,3 @@
 """OpenG2G: GPU-to-Grid framework for distribution-level voltage regulation."""
 
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("openg2g")
-except PackageNotFoundError:
-    __version__ = "0.0.0+unknown"
+__version__ = "0.0.1"
