fb6c01c09bada605cc3a23b5d6fa1c046fb47744
Jae-Won Chung <jwnchung@umich.edu>
2026-03-04 21:53:41 -0500

    CI: Fix lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,10 +8,10 @@ if [[ "$1" == "--fix" ]]; then
 fi
 
 if [[ -z $GITHUB_ACTION ]]; then
-  ruff format openg2g tests examples data
+  ruff format openg2g tests examples
 else
-  ruff format --check openg2g tests examples data
+  ruff format --check openg2g tests examples
 fi
 
-ruff check $FIX_FLAG openg2g tests examples data
-ty check openg2g tests examples data
+ruff check $FIX_FLAG openg2g tests examples
+ty check openg2g tests examples
